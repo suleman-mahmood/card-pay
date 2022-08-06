f9b82a3faf1593d1de29339fc54934d77c0df0b6
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-08-07 02:29:46 +0500

    removed comments

--- frontend-flutter/lib/services/firestore.dart
+++ frontend-flutter/lib/services/firestore.dart
@@ -15,23 +15,4 @@ class FirestoreService {
     var user = model.User.fromJson(data ?? {});
     return user;
   }
-
-  // Future<List<model.Transaction>> getTransactions() async {
-  //   final uid = AuthService().user!.uid;
-
-  //   final ref = _db.collection('transactions');
-
-  //   final q1 = ref.where('sender', isEqualTo: uid);
-  //   final q2 = ref.where('recipient', isEqualTo: uid);
-
-  //   var snapshot = await q1.get();
-  //   var data = snapshot.docs.map((s) => s.data());
-  //   var transactions = data.map((d) => model.Transaction.fromJson(d));
-
-  //   var snapshot2 = await q2.get();
-  //   var data2 = snapshot2.docs.map((s) => s.data());
-  //   var transactions2 = data2.map((d) => model.Transaction.fromJson(d));
-
-  //   return transactions.toList() + transactions2.toList();
-  // }
 }
