03facc7084e6cd9cdd931b2610599097bca3baf3
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-08-15 23:30:00 +0500

    Added scope to error provider on login and signup (#20)

--- frontend-flutter/lib/login/login.dart
+++ frontend-flutter/lib/login/login.dart
@@ -50,132 +50,141 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (context.watch<model.Loading>().getLoading) {
-      return const LoadingWidget();
-    }
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<model.ErrorModel>(
+            create: (_) => model.ErrorModel(),
+          ),
+        ],
+        builder: (context, child) {
+          if (context.watch<model.Loading>().getLoading) {
+            return const LoadingWidget();
+          }
 
-    return Scaffold(
-      body: Center(
-        child: Form(
-          key: _signInFormKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Welcome',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Container(
-                width: 250,
-                child: Text(
-                  'Sign in to continue',
-                  style: Theme.of(context).textTheme.bodyText2,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                width: 250,
-                margin: const EdgeInsets.only(top: 15),
-                padding: const EdgeInsets.only(left: 20),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  color: Colors.orange[700],
-                ),
-                child: TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  onChanged: (rollNumberValue) =>
-                      rollNumber.setRollNumber = rollNumberValue,
-                  decoration: const InputDecoration(
-                    labelText: 'University Roll Number',
-                    border: InputBorder.none,
-                  ),
-                  validator: (rollNumberValue) {
-                    if (rollNumberValue == null) {
-                      return "Please enter Roll Number";
-                    }
-                    if (!rollNumberValue.isValidRollNumber) {
-                      return "Invalid ID";
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Container(
-                width: 250,
-                margin: const EdgeInsets.only(top: 15),
-                padding: const EdgeInsets.only(left: 20),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  color: Colors.orange[700],
-                ),
-                child: TextFormField(
-                  obscureText: true,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  onChanged: (passwordValue) => password = passwordValue,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-              const err.ErrorWidget(),
-              Container(
-                width: 250,
-                padding: const EdgeInsets.only(top: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: () =>
-                          Navigator.pushNamed(context, '/forgot-password'),
-                      child: Text(
-                        'Forgot Password?',
-                        style: Theme.of(context).textTheme.bodyText2,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 250,
-                margin: const EdgeInsets.only(top: 10),
-                child: MaterialButton(
-                  color: Colors.orange[800],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  onPressed: () => _submit(context),
-                  child: const Text(
-                    'Sign In',
-                  ),
-                ),
-              ),
-              Container(
-                width: 250,
-                margin: const EdgeInsets.only(top: 10),
-                child: Row(
+          return Scaffold(
+            body: Center(
+              child: Form(
+                key: _signInFormKey,
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text(
-                      "Don't have an account? ",
+                    Text(
+                      'Welcome',
+                      style: Theme.of(context).textTheme.headline5,
                     ),
-                    GestureDetector(
-                      onTap: () => Navigator.pushNamed(context, '/signup'),
-                      child: const Text(
-                        "Sign Up",
+                    Container(
+                      width: 250,
+                      child: Text(
+                        'Sign in to continue',
+                        style: Theme.of(context).textTheme.bodyText2,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      width: 250,
+                      margin: const EdgeInsets.only(top: 15),
+                      padding: const EdgeInsets.only(left: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        color: Colors.orange[700],
+                      ),
+                      child: TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        onChanged: (rollNumberValue) =>
+                            rollNumber.setRollNumber = rollNumberValue,
+                        decoration: const InputDecoration(
+                          labelText: 'University Roll Number',
+                          border: InputBorder.none,
+                        ),
+                        validator: (rollNumberValue) {
+                          if (rollNumberValue == null) {
+                            return "Please enter Roll Number";
+                          }
+                          if (!rollNumberValue.isValidRollNumber) {
+                            return "Invalid ID";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 250,
+                      margin: const EdgeInsets.only(top: 15),
+                      padding: const EdgeInsets.only(left: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        color: Colors.orange[700],
+                      ),
+                      child: TextFormField(
+                        obscureText: true,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        onChanged: (passwordValue) => password = passwordValue,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const err.ErrorWidget(),
+                    Container(
+                      width: 250,
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () => Navigator.pushNamed(
+                                context, '/forgot-password'),
+                            child: Text(
+                              'Forgot Password?',
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 250,
+                      margin: const EdgeInsets.only(top: 10),
+                      child: MaterialButton(
+                        color: Colors.orange[800],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onPressed: () => _submit(context),
+                        child: const Text(
+                          'Sign In',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 250,
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account? ",
+                          ),
+                          GestureDetector(
+                            onTap: () =>
+                                Navigator.pushNamed(context, '/signup'),
+                            child: const Text(
+                              "Sign Up",
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
--- frontend-flutter/lib/signup/signup.dart
+++ frontend-flutter/lib/signup/signup.dart
@@ -76,185 +76,195 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (context.watch<model.Loading>().getLoading) {
-      return const LoadingWidget();
-    }
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<model.ErrorModel>(
+          create: (_) => model.ErrorModel(),
+        ),
+      ],
+      builder: (context, child) {
+        if (context.watch<model.Loading>().getLoading) {
+          return const LoadingWidget();
+        }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            Container(
-              width: 250,
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-                style: Theme.of(context).textTheme.bodyText2,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Form(
-              key: _signInFormKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 250,
-                    margin: const EdgeInsets.only(top: 15),
-                    padding: const EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                      color: Colors.orange[700],
-                    ),
-                    child: TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      onChanged: (nameValue) => fullName = nameValue,
-                      decoration: const InputDecoration(
-                        labelText: 'Full Name',
-                        border: InputBorder.none,
-                      ),
-                      validator: (nameValue) {
-                        if (nameValue == null) {
-                          return "Please enter your name";
-                        }
-                        if (!nameValue.isValidName) {
-                          return "Please enter your first name and last name";
-                        }
-                        return null;
-                      },
-                    ),
+        return Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Container(
+                  width: 250,
+                  child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                    style: Theme.of(context).textTheme.bodyText2,
+                    textAlign: TextAlign.center,
                   ),
-                  Container(
-                    width: 250,
-                    margin: const EdgeInsets.only(top: 15),
-                    padding: const EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(10),
+                ),
+                Form(
+                  key: _signInFormKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 250,
+                        margin: const EdgeInsets.only(top: 15),
+                        padding: const EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: Colors.orange[700],
+                        ),
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (nameValue) => fullName = nameValue,
+                          decoration: const InputDecoration(
+                            labelText: 'Full Name',
+                            border: InputBorder.none,
+                          ),
+                          validator: (nameValue) {
+                            if (nameValue == null) {
+                              return "Please enter your name";
+                            }
+                            if (!nameValue.isValidName) {
+                              return "Please enter your first name and last name";
+                            }
+                            return null;
+                          },
+                        ),
                       ),
-                      color: Colors.orange[700],
-                    ),
-                    child: TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      onChanged: (rollNumberValue) =>
-                          rollNumber.setRollNumber = rollNumberValue,
-                      decoration: const InputDecoration(
-                        labelText: 'LUMS Roll number',
-                        border: InputBorder.none,
+                      Container(
+                        width: 250,
+                        margin: const EdgeInsets.only(top: 15),
+                        padding: const EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: Colors.orange[700],
+                        ),
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (rollNumberValue) =>
+                              rollNumber.setRollNumber = rollNumberValue,
+                          decoration: const InputDecoration(
+                            labelText: 'LUMS Roll number',
+                            border: InputBorder.none,
+                          ),
+                          validator: (rollNumberValue) {
+                            if (rollNumberValue == null) {
+                              return "Please enter Roll Number";
+                            }
+                            if (!rollNumberValue.isValidRollNumber) {
+                              return "Invalid ID";
+                            }
+                            return null;
+                          },
+                        ),
                       ),
-                      validator: (rollNumberValue) {
-                        if (rollNumberValue == null) {
-                          return "Please enter Roll Number";
-                        }
-                        if (!rollNumberValue.isValidRollNumber) {
-                          return "Invalid ID";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Container(
-                    width: 250,
-                    margin: const EdgeInsets.only(top: 15),
-                    padding: const EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(10),
+                      Container(
+                        width: 250,
+                        margin: const EdgeInsets.only(top: 15),
+                        padding: const EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: Colors.orange[700],
+                        ),
+                        child: TextFormField(
+                          obscureText: true,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (passwordValue) =>
+                              password = passwordValue,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            border: InputBorder.none,
+                          ),
+                          validator: (passwordValue) {
+                            if (passwordValue == null) {
+                              return "Please enter Password";
+                            }
+                            if (!passwordValue.isValidPassword) {
+                              return "Weak Password";
+                            }
+                            return null;
+                          },
+                        ),
                       ),
-                      color: Colors.orange[700],
-                    ),
-                    child: TextFormField(
-                      obscureText: true,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      onChanged: (passwordValue) => password = passwordValue,
-                      decoration: const InputDecoration(
-                        labelText: 'Password',
-                        border: InputBorder.none,
+                      Container(
+                        width: 250,
+                        margin: const EdgeInsets.only(top: 15),
+                        padding: const EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: Colors.orange[700],
+                        ),
+                        child: TextFormField(
+                          obscureText: true,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (confirmPasswordValue) =>
+                              confirmPassword = confirmPasswordValue,
+                          decoration: const InputDecoration(
+                            labelText: 'Confirm Password',
+                            border: InputBorder.none,
+                          ),
+                          validator: (confirmPasswordValue) {
+                            if (confirmPasswordValue == null) {
+                              return "Please re-enter Password";
+                            }
+                            if (confirmPasswordValue != password) {
+                              return "The passwords don't match";
+                            }
+                            return null;
+                          },
+                        ),
                       ),
-                      validator: (passwordValue) {
-                        if (passwordValue == null) {
-                          return "Please enter Password";
-                        }
-                        if (!passwordValue.isValidPassword) {
-                          return "Weak Password";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Container(
-                    width: 250,
-                    margin: const EdgeInsets.only(top: 15),
-                    padding: const EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(10),
+                      const err.ErrorWidget(),
+                      Container(
+                        margin: EdgeInsets.only(top: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Checkbox(value: false, onChanged: (e) => true),
+                            Container(
+                              width: 200,
+                              child: Text(
+                                'By creating your account you agree with  our Terms and Conditions',
+                                style: Theme.of(context).textTheme.bodyText2,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      color: Colors.orange[700],
-                    ),
-                    child: TextFormField(
-                      obscureText: true,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      onChanged: (confirmPasswordValue) =>
-                          confirmPassword = confirmPasswordValue,
-                      decoration: const InputDecoration(
-                        labelText: 'Confirm Password',
-                        border: InputBorder.none,
-                      ),
-                      validator: (confirmPasswordValue) {
-                        if (confirmPasswordValue == null) {
-                          return "Please re-enter Password";
-                        }
-                        if (confirmPasswordValue != password) {
-                          return "The passwords don't match";
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  const err.ErrorWidget(),
-                  Container(
-                    margin: EdgeInsets.only(top: 15),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Checkbox(value: false, onChanged: (e) => true),
-                        Container(
-                          width: 200,
-                          child: Text(
-                            'By creating your account you agree with  our Terms and Conditions',
-                            style: Theme.of(context).textTheme.bodyText2,
+                      Container(
+                        width: 250,
+                        margin: const EdgeInsets.only(top: 10),
+                        child: MaterialButton(
+                          color: Colors.orange[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          onPressed: () => _submit(context),
+                          child: const Text(
+                            'Sign Up',
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 250,
-                    margin: const EdgeInsets.only(top: 10),
-                    child: MaterialButton(
-                      color: Colors.orange[800],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
                       ),
-                      onPressed: () => _submit(context),
-                      child: const Text(
-                        'Sign Up',
-                      ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
