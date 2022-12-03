eb63e7f38394628d9f9293c569ea751619dbe27c
Suleman Mahmood <sulemanmahmood99@gmail.com>
2022-12-04 01:24:37 +0500

    new data report generated

--- admin/data_analytics.py
+++ admin/data_analytics.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 
 
-with open("2022-11-07 17:45:03_DBState.json") as f:
+with open("2022-12-02 19:16:18_DBState.json") as f:
     data = json.load(f)
 
 df = pd.DataFrame.from_dict(data["transactions"], "index")
