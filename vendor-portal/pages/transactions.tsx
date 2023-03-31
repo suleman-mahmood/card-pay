d412aaa8aaff295226c7883f80c7b690e308ed8e
Suleman Mahmood <sulemanmahmood99@gmail.com>
2023-04-01 02:52:03 +0500

    copy pasted code to other pages (#89)

--- vendor-portal/pages/transactions.tsx
+++ vendor-portal/pages/transactions.tsx
@@ -9,7 +9,8 @@ import Navbar from '../components/navbar';
 import { httpsCallable } from 'firebase/functions';
 import Swal from 'sweetalert2';
 import Loader from '../components/loader';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { FirebaseError } from 'firebase/app';
 
 interface userDataDoc {
 	balance: number;
@@ -33,8 +34,31 @@ const Transactions: NextPage = () => {
 	const router = useRouter();
 
 	const [loading, setLoading] = useState(true);
+	const [errorMessage, setErrorMessage] = useState('');
+
 	const [user, setUser] = useState<FirebaseUser | null>(null);
 	const [userData, setUserData] = useState<userDataDoc | null>(null);
+
+	const [playAudio, setPlayAudio] = useState(false);
+	const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
+	const [order, setOrder] = useState<{
+		[uid: string]: {
+			orderId: string;
+			cart: Array<{
+				restaurantId: string;
+				name: string;
+				price: number;
+				quantity: number;
+			}>;
+			specialInstructions: string;
+			isDelivery: boolean;
+			customerName: string;
+			customerRollNumber: string;
+			contactNumber: string;
+			deliveryAddress: string;
+			timestamp: number;
+		};
+	}>();
 
 	useEffect(() => {
 		return onAuthStateChanged(auth, (user) => {
@@ -106,6 +130,149 @@ const Transactions: NextPage = () => {
 		));
 	};
 
+	/*
+		Pre order stuff
+	*/
+	useEffect(() => {
+		if (user === null) {
+			return;
+		}
+		const restaurantId = user.uid;
+		return onSnapshot(
+			doc(db, 'new_order_requests', restaurantId),
+			async (d) => {
+				if (Object.keys(d.data()?.orders).length !== 0) {
+					setPlayAudio(true);
+				} else {
+					setPlayAudio(false);
+				}
+				setOrder(d.data()?.orders);
+				console.log(d.data()?.orders);
+			}
+		);
+	}, [user]);
+
+	useEffect(() => {
+		// Play audio
+		const play = async () => {
+			let audio = new Audio(
+				'https://assets.mixkit.co/active_storage/sfx/940/940-preview.mp3'
+			);
+
+			await audio.play();
+			let id = setInterval(async () => {
+				await audio.play();
+			}, 1500);
+			setIntervalId(id);
+		};
+
+		if (playAudio) {
+			play();
+		} else {
+			clearInterval(intervalId);
+		}
+	}, [playAudio]);
+
+	const handleOpenOrderRequest = async (uid: string) => {
+		setPlayAudio(false);
+
+		if (order === undefined || user == null) {
+			return;
+		}
+		const restaurantId = user.uid;
+		const orderType = order[uid].isDelivery ? 'Delivery' : 'Pick-up';
+		const amount = getBalance(order[uid].cart);
+		const n = order[uid].customerName;
+		const r = order[uid].customerRollNumber;
+
+		let textData = `<p>${n} ${r}</p>`;
+		textData += `<p>Order type: ${orderType}</p>`;
+
+		if (order[uid].isDelivery) {
+			textData += `<p>Contact: ${order[uid].contactNumber}</p>`;
+			textData += `<p>Address: ${order[uid].deliveryAddress}</p>`;
+		}
+
+		const itemsList = order[uid].cart
+			.map((i) => `${i.name}: ${i.quantity}`)
+			.join(`</p><p>`);
+		textData += `<p>${itemsList}</p>`;
+		textData += `<p>Total: ${amount}</p>`;
+
+		const si = order[uid].specialInstructions;
+		if (si.length !== 0) {
+			textData += `<p>Special instructions: ${si}</p>`;
+		}
+
+		const res = await Swal.fire({
+			title: 'Order Details',
+			html: textData,
+			showDenyButton: true,
+			confirmButtonText: 'Accept',
+			denyButtonText: `Reject`,
+		});
+
+		if (res.isConfirmed) {
+			try {
+				const confirmPickupOrder = httpsCallable(
+					functions,
+					'confirmPickupOrder'
+				);
+				await confirmPickupOrder({
+					cart: order[uid].cart,
+					orderId: order[uid].orderId,
+					restaurantId: restaurantId,
+					customerUid: uid,
+				});
+				Swal.fire({
+					icon: 'success',
+					title: 'Order Accpeted',
+				});
+			} catch (error) {
+				const message = (error as FirebaseError).message;
+				setErrorMessage(message);
+				console.log(error);
+				Swal.fire({
+					icon: 'error',
+					title: 'Order Declined',
+					text: message,
+				});
+			}
+		}
+		if (res.isDenied) {
+			try {
+				const denyPickupOrder = httpsCallable(
+					functions,
+					'denyPickupOrder'
+				);
+				await denyPickupOrder({
+					cart: order[uid].cart,
+					orderId: order[uid].orderId,
+					restaurantId: restaurantId,
+					customerUid: uid,
+				});
+			} catch (error) {
+				setErrorMessage((error as FirebaseError).message);
+				console.log(error);
+			}
+		}
+	};
+
+	const getBalance = (
+		cart: Array<{
+			quantity: number;
+			price: number;
+		}>
+	): number => {
+		let balance = 0;
+
+		cart.forEach((c) => {
+			balance += c.quantity * c.price;
+		});
+
+		return balance;
+	};
+
 	return loading ? (
 		<Loader />
 	) : (
@@ -117,6 +284,55 @@ const Transactions: NextPage = () => {
 						<h1 className='mb-4 text-5xl font-bold'>
 							Transactions
 						</h1>
+
+						{/* Pre order stuff */}
+						<div className='stack'>
+							{order !== undefined
+								? Object.keys(order).map((uid) => (
+										<div
+											className='mb-4 card bg-white text-black shadow-xl'
+											onClick={() =>
+												handleOpenOrderRequest(uid)
+											}
+											key={uid}
+										>
+											<div className='card-body'>
+												{order[uid].cart.map(
+													(item, i) => (
+														<p key={i}>
+															{item.name}:{' '}
+															{item.quantity}
+														</p>
+													)
+												)}
+											</div>
+										</div>
+								  ))
+								: null}
+						</div>
+
+						{/* Error dialogue box */}
+						{errorMessage === '' ? null : (
+							<div className='mt-6 alert alert-error shadow-lg'>
+								<div>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										className='stroke-current flex-shrink-0 h-6 w-6'
+										fill='none'
+										viewBox='0 0 24 24'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+										/>
+									</svg>
+									<span>Error! {errorMessage}</span>
+								</div>
+							</div>
+						)}
+
 						<p>{userData?.fullName}</p>
 						<p className='mb-8'>Balance: {userData?.balance}</p>
 
