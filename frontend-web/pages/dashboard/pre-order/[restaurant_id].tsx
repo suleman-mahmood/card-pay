cb7f549660d69fff062fd3eaa6cb3eecd3f9a519
Suleman Mahmood <sulemanmahmood99@gmail.com>
2023-04-01 06:09:17 +0500

    Layer 2 menu (#88)
    * implemented bottom popup
    
    * completed implementation
    
    * removed dev env

--- frontend-web/pages/dashboard/pre-order/[restaurant_id].tsx
+++ frontend-web/pages/dashboard/pre-order/[restaurant_id].tsx
@@ -19,7 +19,34 @@ interface RestaurantDetails {
 		price: number;
 		category: string;
 		description: string;
+		variantsExist: boolean;
+		variants: Array<{
+			name: string;
+			price: number;
+			category: string;
+		}>;
 	}>;
+}
+
+interface MenuItem {
+	name: string;
+	price: number;
+}
+
+interface MenuCategory {
+	[key: string]: MenuItem[];
+}
+
+interface SelectedOptions {
+	[key: string]: {
+		isSelected: string | boolean;
+		price: number;
+		optional: boolean;
+	};
+}
+
+interface Props {
+	newMenu: MenuCategory;
 }
 
 const restaurant_map = new Map<string, string>([
@@ -43,8 +70,22 @@ const DigitalCard: NextPage = () => {
 
 	const [errorMessage, setErrorMessage] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
+	const [shouldDisplaySecondaryMenu, setShouldDisplaySecondaryMenu] =
+		useState(false);
 
 	const [menu, setMenu] = useState<RestaurantDetails>();
+	const [selectedItemVariant, setSelectedItemVariant] = useState<{
+		name: string;
+		price: number;
+		description: string;
+		variantsExist: boolean;
+		variants: Array<{
+			name: string;
+			price: number;
+			category: string;
+		}>;
+	}>();
+	const [selectedOptions, setSelectedOptions] = useState<SelectedOptions>({});
 
 	useEffect(() => {
 		const fetchRestaurantMenu = async () => {
@@ -56,7 +97,6 @@ const DigitalCard: NextPage = () => {
 			const docData = docSnap.data() as RestaurantDetails;
 
 			setMenu(docData);
-			console.log(docData);
 		};
 
 		fetchRestaurantMenu();
@@ -104,12 +144,43 @@ const DigitalCard: NextPage = () => {
 		}
 	};
 
+	const handleAddClick = (item: {
+		name: string;
+		price: number;
+		description: string;
+		variantsExist: boolean;
+		variants: Array<{
+			name: string;
+			price: number;
+			category: string;
+		}>;
+	}) => {
+		if (item.variantsExist === undefined) {
+			addToCart(item);
+			return;
+		}
+
+		if (!item.variantsExist) {
+			addToCart(item);
+			return;
+		}
+
+		setSelectedItemVariant(item);
+		setShouldDisplaySecondaryMenu(true);
+	};
+
 	const displayMenu = () => {
 		const newMenu: {
 			[category: string]: Array<{
 				name: string;
 				price: number;
 				description: string;
+				variantsExist: boolean;
+				variants: Array<{
+					name: string;
+					price: number;
+					category: string;
+				}>;
 			}>;
 		} = {};
 
@@ -138,7 +209,7 @@ const DigitalCard: NextPage = () => {
 								<div className='grow'></div>
 								<button
 									className='btn btn-primary bg-gradient-to-l from-primary to-primarydark border-none text-2xl mt-2 text-white rounded-full'
-									onClick={() => addToCart(item)}
+									onClick={() => handleAddClick(item)}
 								>
 									+
 								</button>
@@ -159,6 +230,169 @@ const DigitalCard: NextPage = () => {
 				))}
 			</div>
 		));
+	};
+
+	const handleRadioChange = (
+		event: React.ChangeEvent<HTMLInputElement>,
+		item: MenuItem
+	): void => {
+		const name: string = event.target.name;
+		const value: string = event.target.value;
+		setSelectedOptions((prevState) => ({
+			...prevState,
+			[name]: { isSelected: value, price: item.price, optional: false },
+		}));
+	};
+
+	const handleCheckboxChange = (
+		event: React.ChangeEvent<HTMLInputElement>,
+		item: MenuItem
+	): void => {
+		const name: string = event.target.value;
+		const checked: boolean = event.target.checked;
+		setSelectedOptions((prevState) => ({
+			...prevState,
+			[name]: { isSelected: checked, price: item.price, optional: true },
+		}));
+	};
+
+	const displaySecondaryMenu = () => {
+		const newMenu: {
+			[category: string]: Array<{
+				name: string;
+				price: number;
+				category: string;
+			}>;
+		} = {};
+
+		selectedItemVariant?.variants.forEach((item) => {
+			if (item.category in newMenu) {
+				newMenu[item.category].push(item);
+			} else {
+				newMenu[item.category] = [item];
+			}
+		});
+
+		return Object.keys(newMenu).map((key) => (
+			<div key={key}>
+				<p className='text-2xl mt-4 font-bold text-left'>{key}</p>
+				<div className='flex flex-col gap-2'>
+					<div className='flex flex-col gap-2'></div>
+					{key !== 'optional' ? (
+						<div>
+							{newMenu[key].map((item: MenuItem) => (
+								<label
+									key={item.name}
+									className='flex items-center'
+								>
+									<input
+										type='radio'
+										value={item.name}
+										className='mr-2'
+										name={key}
+										checked={
+											selectedOptions[key] &&
+											selectedOptions[key].isSelected ===
+												item.name
+										}
+										onChange={(e) =>
+											handleRadioChange(e, item)
+										}
+									/>
+									{item.name}: Rs.{item.price}
+								</label>
+							))}
+						</div>
+					) : (
+						<div>
+							{newMenu[key].map((item: MenuItem) => (
+								<label
+									key={item.name}
+									className='flex items-center'
+								>
+									<input
+										type='checkbox'
+										value={item.name}
+										className='mr-2'
+										checked={
+											selectedOptions[item.name] &&
+											(selectedOptions[item.name]
+												.isSelected as boolean)
+										}
+										onChange={(e) =>
+											handleCheckboxChange(e, item)
+										}
+									/>
+									{item.name}: Rs.{item.price}
+								</label>
+							))}
+						</div>
+					)}
+				</div>
+			</div>
+		));
+	};
+
+	const handleAddToCart = () => {
+		setErrorMessage('');
+
+		let categories: string[] = [];
+		selectedItemVariant?.variants.forEach((item) => {
+			if (
+				item.category !== 'optional' &&
+				categories.indexOf(item.category) === -1
+			) {
+				categories.push(item.category);
+			}
+		});
+		const uniqueItems = categories.length;
+
+		let selectedUniqueItems = 0;
+		Object.keys(selectedOptions).map((key) => {
+			const option = selectedOptions[key];
+
+			if (
+				option.optional ||
+				(typeof option.isSelected === 'boolean' &&
+					option.isSelected === false)
+			) {
+				return;
+			}
+
+			selectedUniqueItems += 1;
+		});
+
+		if (uniqueItems !== selectedUniqueItems) {
+			setErrorMessage('Please select all required items!');
+			return;
+		}
+
+		let newName = `${selectedItemVariant!.name}`;
+		let total = selectedItemVariant!.price;
+
+		Object.keys(selectedOptions).map((key) => {
+			const option = selectedOptions[key];
+
+			if (typeof option.isSelected !== 'boolean') {
+				total += option.price;
+				newName += `, ${key}: ${option.isSelected}`;
+			}
+		});
+
+		Object.keys(selectedOptions).map((key) => {
+			const option = selectedOptions[key];
+
+			if (
+				typeof option.isSelected === 'boolean' &&
+				option.isSelected === true
+			) {
+				total += option.price;
+				newName += `, ${key}`;
+			}
+		});
+
+		addToCart({ name: newName, price: total });
+		setShouldDisplaySecondaryMenu(false);
 	};
 
 	return isLoading ? (
@@ -186,8 +420,66 @@ const DigitalCard: NextPage = () => {
 				{displayMenu()}
 			</div>
 
+			{shouldDisplaySecondaryMenu ? (
+				<div className='fixed z-10 inset-0 overflow-y-auto'>
+					<div className='flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center'>
+						<div
+							className='fixed inset-0 transition-opacity'
+							aria-hidden='true'
+						>
+							<div className='absolute inset-0 bg-gray-500 opacity-75'></div>
+						</div>
+
+						<div className='w-full inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6'>
+							<div className='sm:flex sm:items-start'>
+								<div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+									<h3 className='text-lg text-center leading-6 font-medium text-gray-900'>
+										{selectedItemVariant?.name}: Rs.
+										{selectedItemVariant?.price}
+									</h3>
+									<div className='mt-2'>
+										<p className='text-sm leading-5 text-gray-500'>
+											{selectedItemVariant?.description}
+										</p>
+										{displaySecondaryMenu()}
+										<ErrorAlert message={errorMessage} />
+									</div>
+								</div>
+							</div>
+
+							<div className='mt-5 sm:mt-4 sm:flex sm:flex-row-reverse'>
+								<span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
+									<button
+										type='button'
+										className='inline-flex btn-primary justify-center w-full rounded-md border border-transparent px-4 py-2 text-base leading-6 font-medium text-white shadow-sm  focus:outline-none  focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+										onClick={handleAddToCart}
+									>
+										Add to cart
+									</button>
+								</span>
+
+								<span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
+									<button
+										type='button'
+										className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5'
+										onClick={() => {
+											setShouldDisplaySecondaryMenu(
+												false
+											);
+											setErrorMessage('');
+										}}
+									>
+										Cancel
+									</button>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			) : null}
+
 			{/* Cart button below */}
-			{cartState.itemsCount !== 0 ? (
+			{!shouldDisplaySecondaryMenu && cartState.itemsCount !== 0 ? (
 				<div className='fixed bottom-4 left-0 w-full '>
 					<button
 						className='w-3/4 btn btn-primary bg-gradient-to-l from-primary to-primarydark border-hidden shadow-sm text-white'
