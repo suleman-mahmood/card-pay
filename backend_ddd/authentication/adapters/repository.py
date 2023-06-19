0c93406d78a5b6f2b7f9ccfd6f4fdfbd4553bb4f
Shaheer-Ahmd <shaheerahmad57575@gmail.com>
2023-06-19 15:22:08 +0500

    Formatted

--- backend_ddd/authentication/adapters/repository.py
+++ backend_ddd/authentication/adapters/repository.py
@@ -80,102 +80,252 @@ class ClosedLoopRepository(ClosedLoopAbstractRepository):
 
     def add(self, closed_loop: ClosedLoop):
 
-        sql = "INSERT INTO closed_loops (id, name, logo_url, description, regex, verification_type) VALUES (%s, %s, %s, %s, %s, %s)"
+        sql = '''
+            insert into closed_loops (id, name, logo_url, description, regex,verification_type)
+            values (%s, %s, %s, %s, %s, %s)
+        '''
 
-        self.cursor.execute(sql, (closed_loop.id, closed_loop.name, closed_loop.logo_url,
-                            closed_loop.description, closed_loop.regex, closed_loop.verification_type))
+        self.cursor.execute(
+            sql,
+            [
+                closed_loop.id,
+                closed_loop.name,
+                closed_loop.logo_url,
+                closed_loop.description,
+                closed_loop.regex,
+                closed_loop.verification_type
+            ],
+        )
 
     def get(self, closed_loop_id: str) -> ClosedLoop:
 
-        sql = "SELECT id, name, logo_url, description, regex, verification_type, created_at FROM closed_loops WHERE id = %s"
+        sql = """
+        select id, name, logo_url, description, regex, verification_type, created_at
+        from closed_loops
+        where id = %s
+        """
 
-        self.cursor.execute(sql, (closed_loop_id,))
+        self.cursor.execute(
+            sql,
+            [
+                closed_loop_id,
+            ],
+        )
 
         row = self.cursor.fetchone()
 
-        return ClosedLoop(id=row[0], name=row[1], logo_url=row[2], description=row[3], regex=row[4], verification_type=row[5], created_at=row[6])
+        return ClosedLoop(
+            id=row[0],
+            name=row[1],
+            logo_url=row[2],
+            description=row[3],
+            regex=row[4],
+            verification_type=row[5],
+            created_at=row[6]
+        )
 
     def save(self, closed_loop: ClosedLoop):
-            
-        sql = '''INSERT into closed_loops SET name = %s, logo_url = %s, description = %s, regex = %s, verification_type = %s WHERE id = %s 
-        on conflict(id) do update set 
-        name = excluded.name,
-        logo_url = excluded.logo_url,
-        description = excluded.description,
-        regex = excluded.regex,
-        verification_type = excluded.verification_type)'''
 
-        self.cursor.execute(sql, (closed_loop.name, closed_loop.logo_url,closed_loop.description, closed_loop.regex, closed_loop.verification_type, closed_loop.id))
+        sql = """
+            insert into closed_loops (id, name, logo_url, description, regex, verification_type)
+            values (%s, %s, %s, %s, %s, %s)
+            where id = %s 
+            on conflict(id) do update set 
+                name = excluded.name,
+                logo_url = excluded.logo_url,
+                description = excluded.description,
+                regex = excluded.regex,
+                verification_type = excluded.verification_type)
+        """
+
+        self.cursor.execute(
+            sql,
+            [
+                closed_loop.id,
+                closed_loop.name,
+                closed_loop.logo_url,
+                closed_loop.description,
+                closed_loop.regex,
+                closed_loop.verification_type
+            ]
+        )
+
 
 class UserRepository(UserAbstractRepository):
 
-    def __init__(self,connection):
+    def __init__(self, connection):
         self.connection = connection
         self.cursor = connection.cursor()
 
     def add(self, user: User):
-        sql = "INSERT INTO users SET id = %s, personal_email = %s, phone_number = %s, user_type = %s, pin = %s, full_name = %s, wallet_id = %s, is_active = %s, is_phone_number_verified = %s, otp = %s, otp_generated_at = %s, location = %s, created_at)"
+        sql = """
+            insert into users (id, personal_email, phone_number, user_type, pin, full_name, wallet_id, is_active, is_phone_number_verified, otp, otp_generated_at, location, created_at) 
+            values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
 
-        self.cursor.execute(sql, (user.id, user.personal_email, user.phone_number, user.user_type, user.pin, user.full_name, user.wallet_id, user.is_active, user.is_phone_number_verified, user.otp, user.otp_generated_at, user.location, user.created_at))
+        self.cursor.execute(
+            sql,
+            [
+                user.id,
+                user.personal_email,
+                user.phone_number,
+                user.user_type,
+                user.pin,
+                user.full_name,
+                user.wallet_id,
+                user.is_active,
+                user.is_phone_number_verified,
+                user.otp,
+                user.otp_generated_at,
+                user.location,
+                user.created_at
+            ]
+        )
 
         for key in user.closed_loops:
-            sql = '''INSERT INTO user_closed_loops (user_id, closed_loop_id, unique_identifier, closed_loop_user_id,
-            unique_identifier_otp, status, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s)'''
-            
+            sql = """
+                insert into user_closed_loops (user_id, closed_loop_id, unique_identifier, closed_loop_user_id, unique_identifier_otp, status, created_at)
+                values (%s, %s, %s, %s, %s, %s, %s)
+            """
+
             closed_loop_user = user.closed_loops[key]
 
-            self.cursor.execute(sql, (user.id, key, closed_loop_user.unique_identifier, closed_loop_user.id, closed_loop_user.unique_identifier_otp, closed_loop_user.status, closed_loop_user.created_at))
+            self.cursor.execute(
+                sql,
+                [
+                    user.id,
+                    key,
+                    closed_loop_user.unique_identifier,
+                    closed_loop_user.id,
+                    closed_loop_user.unique_identifier_otp,
+                    closed_loop_user.status,
+                    closed_loop_user.created_at
+                ]
+            )
 
     def get(self, user_id: str) -> User:
-        sql = "SELECT id, personal_email, phone_number, user_type, pin, full_name, wallet_id, is_active, is_phone_number_verified, closed_loops, otp, otp_generated_at, location, created_at FROM users WHERE id = %s"
+        sql = """
+        select id, personal_email, phone_number, user_type, pin, full_name, wallet_id, is_active, is_phone_number_verified, closed_loops, otp, otp_generated_at, location, created_at 
+        from users 
+        where id = %s
+        """
 
-        self.cursor.execute(sql, (user_id,))
+        self.cursor.execute(
+            sql,
+            [
+                user_id
+            ]
+        )
 
         row = self.cursor.fetchone()
 
-        user = User(id=row[0], personal_email=row[1], phone_number=row[2], user_type=row[3], pin=row[4], full_name=row[5], wallet_id=row[6], is_active=row[7], is_phone_number_verified=row[8], closed_loops = {}, otp=row[9], otp_generated_at=row[10], location=row[11], created_at=row[12])
-        
-        sql = "SELECT user_id, closed_loop_id, unique_identifier, closed_loop_user_id, unique_identifier_otp, status, created_at FROM user_closed_loops WHERE user_id = %s"
+        user = User(
+            id=row[0], 
+            personal_email=row[1], 
+            phone_number=row[2], 
+            user_type=row[3], 
+            pin=row[4], 
+            full_name=row[5], 
+            wallet_id=row[6],
+            is_active=row[7], 
+            is_phone_number_verified=row[8], 
+            closed_loops={}, 
+            otp=row[9], 
+            otp_generated_at=row[10], 
+            location=row[11], 
+            created_at=row[12]
+            )
 
-        self.cursor.execute(sql, (user_id,))
+        sql = """
+        select user_id, closed_loop_id, unique_identifier, closed_loop_user_id, unique_identifier_otp, status, created_at 
+        from user_closed_loops 
+        where user_id = %s
+        """
+
+        self.cursor.execute(
+            sql,
+            [
+                user_id
+            ]
+        )
 
         rows = self.cursor.fetchall()
 
         for row in rows:
-            closed_loop_user = ClosedLoopUser(closed_loop_id = row[1], unique_identifier = row[2], id = row[3], unique_identifier_otp = row[4], status = row[5], created_at = row[6])
+            closed_loop_user = ClosedLoopUser(
+                closed_loop_id=row[1],
+                unique_identifier=row[2], 
+                id=row[3], 
+                unique_identifier_otp=row[4], 
+                status=row[5], 
+                created_at=row[6]
+            )
 
             user.closed_loops[row[1]] = closed_loop_user
 
         return user
 
     def save(self, user: User):
-        sql = '''INSERT into users SET personal_email = %s, phone_number = %s, user_type = %s, pin = %s, full_name = %s, wallet_id = %s, is_active = %s, is_phone_number_verified = %s, otp = %s, otp_generated_at = %s, location = %s WHERE id = %s on conflict(id) do update set
-        personal_email = excluded.personal_email,
-        phone_number = excluded.phone_number,
-        user_type = excluded.user_type,
-        pin = excluded.pin,
-        full_name = excluded.full_name,
-        wallet_id = excluded.wallet_id,
-        is_active = excluded.is_active,
-        is_phone_number_verified = excluded.is_phone_number_verified,
-        otp = excluded.otp,
-        otp_generated_at = excluded.otp_generated_at,
-        location = excluded.location
-        )'''
+        sql = """
+        insert into users (id, personal_email, phone_number, user_type, pin, full_name, wallet_id, is_active, is_phone_number_verified, otp, otp_generated_at, location, created_at)
+        values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        where id = %s 
+        on conflict(id) do update set
+            id = excluded.id,
+            personal_email = excluded.personal_email,
+            phone_number = excluded.phone_number,
+            user_type = excluded.user_type,
+            pin = excluded.pin,
+            full_name = excluded.full_name,
+            wallet_id = excluded.wallet_id,
+            is_active = excluded.is_active,
+            is_phone_number_verified = excluded.is_phone_number_verified,
+            otp = excluded.otp,
+            otp_generated_at = excluded.otp_generated_at,
+            location = excluded.location
+        )
+        """
 
-        self.cursor.execute(sql, (user.personal_email, user.phone_number, user.user_type, user.pin, user.full_name, user.wallet_id, user.is_active, user.is_phone_number_verified, user.otp, user.otp_generated_at, user.location, user.id))
+        self.cursor.execute(
+            sql,
+            [
+                user.personal_email, 
+                user.phone_number, 
+                user.user_type, 
+                user.pin, 
+                user.full_name, 
+                user.wallet_id,
+                user.is_active, 
+                user.is_phone_number_verified, 
+                user.otp, 
+                user.otp_generated_at, 
+                user.location, 
+                user.id
+            ]
+        )
 
         for key in user.closed_loops:
-            sql = '''INSERT into user_closed_loops SET unique_identifier = %s, unique_identifier_otp = %s, status = %s WHERE user_id = %s AND closed_loop_id = %s
+            sql = """
+            insert into user_closed_loops (user_id, closed_loop_id, unique_identifier, unique_identifier_otp, status, created_at)
+            values (%s, %s, %s, %s, %s, %s)
             on conflict(user_id, closed_loop_id) do update set
-            unique_identifier = excluded.unique_identifier,
-            unique_identifier_otp = excluded.unique_identifier_otp,
-            status = excluded.status
-            '''
+                user_id = excluded.user_id,
+                closed_loop_id = excluded.closed_loop_id,
+                unique_identifier = excluded.unique_identifier,
+                unique_identifier_otp = excluded.unique_identifier_otp,
+                status = excluded.status,
+                created_at = excluded.created_at
+            """
 
             closed_loop_user = user.closed_loops[key]
 
-            self.cursor.execute(sql, (closed_loop_user.unique_identifier, closed_loop_user.unique_identifier_otp, closed_loop_user.status, user.id, key))
-    
-
-
+            self.cursor.execute(
+                sql,
+                [
+                    closed_loop_user.unique_identifier,
+                    closed_loop_user.unique_identifier_otp, 
+                    closed_loop_user.status, 
+                    user.id, 
+                    key
+                ]
+            )
--- backend_ddd/db/initialize.sql
+++ backend_ddd/db/initialize.sql
@@ -7,6 +7,9 @@ drop type if exists transaction_status_enum;
 create type  transaction_mode_enum as enum ('QR', 'RFID', 'NFC', 'BARCODE', 'APP_TRANSFER');
 create type transaction_type_enum as enum ('POS', 'P2P_PUSH', 'P2P_PULL', 'VOUCHER', 'VIRTUAL_POS', 'PAYMENT_GATEWAY', 'CARD_PAY');
 create type transaction_status_enum as enum ('PENDING', 'FAILED', 'SUCCESSFUL', 'EXPIRED', 'DECLINED');
+create type closed_loop_verification_type as enum ('NONE','ROLLNUMBER','EMAIL','MEMBERSHIP_ID')
+create type user_type_enum as enum ('CUSTOMER','VENDOR','ADMIN','PAYMENT_GATEWAY','CARDPAY')
+create type closed_loop_user_state_enum as enum ('UN_VERIFIED','VERIFIED')
 
 create table wallets (
     id uuid primary key,
@@ -33,25 +36,25 @@ create closed_loops(
     logo_url varchar(255) not null,
     description varchar(255) not null,
     regex varchar(255) not null,
-    verification_type varchar(255) not null,
-    created_at timestamp not null
+    verification_type closed_loop_verification_type not null,
+    created_at timestamp not null default current_timestamp,
 )
 
 create table users (
     id uuid primary key,
     personal_email varchar(255) not null,
     phone_number varchar(255) not null,
-    user_type varchar(255) not null,
+    user_type user_type_enum not null,
     pin varchar(255) not null,
     full_name varchar(255) not null,
     wallet_id uuid References wallets(id) not null,
     is_active boolean not null,
     is_phone_number_verified boolean not null,
   
-    otp varchar(255) not null,
-    otp_generated_at timestamp not null,
-    location point not null,
-    created_at timestamp not null,
+    otp varchar(4) not null,
+    otp_generated_at timestamp not null default current_timestamp,
+    location point not null default point(0,0),
+    created_at timestamp not null default current_timestamp,
     
 
 
@@ -61,12 +64,12 @@ create table user_closed_loops (
     
     user_id uuid references users(id),
     closed_loop_id uuid references closed_loops(id),
-    
+
     unique_indentifier varchar(255) not null,
     closed_loop_user_id varchar(255) not null,
-    unique_indentifier_otp varchar(255) not null,
-    status varchar(255) not null,
-    created_at timestamp not null,
+    unique_indentifier_otp varchar(4) not null,
+    status closed_loop_user_state_enum not null,
+    created_at timestamp not null default current_timestamp,
 
     primary key (user_id, closed_loop_id)
 )
