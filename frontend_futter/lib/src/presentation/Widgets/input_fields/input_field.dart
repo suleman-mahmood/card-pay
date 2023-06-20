d60335c52c978151f83938f8a2716a7925561793
talha-husnain <24100245@lums.edu.pk>
2023-06-20 14:00:57 +0500

    Remove extra fiels

--- frontend_futter/lib/src/presentation/Widgets/input_fields/input_field.dart
+++ frontend_futter/lib/src/presentation/Widgets/input_fields/input_field.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:frontend_futter/src/presentation/Widgets/input_fields/drop_down.dart';
-import 'package:frontend_futter/src/presentation/Widgets/input_fields/input_text_field.dart';
 import 'package:frontend_futter/src/config/themes/colors.dart';
 
 class CustomInputField extends HookWidget {
@@ -9,36 +7,26 @@ class CustomInputField extends HookWidget {
   final String? hint;
   final bool obscureText;
   final FormFieldValidator<String>? validator;
-  final List<String>? dropdownItems;
   final ValueChanged<String?>? onChanged;
   final TextInputType? keyboardType;
-  final Alignment dropdownAlignment;
 
   const CustomInputField({
     required this.label,
     this.hint,
-    this.dropdownItems,
     this.obscureText = false,
     this.validator,
     this.onChanged,
     this.keyboardType,
-    this.dropdownAlignment = Alignment.centerLeft,
   });
 
   @override
   Widget build(BuildContext context) {
-    final selectedDropdownItem = useState<String?>(null);
     final controller = useTextEditingController();
     final passwordVisible = useState<bool>(false);
 
     useEffect(() {
       return controller.dispose;
     }, []);
-
-    void handleDropdownChange(String? newValue) {
-      selectedDropdownItem.value = newValue;
-      onChanged?.call(newValue);
-    }
 
     void togglePasswordVisibility() {
       passwordVisible.value = !passwordVisible.value;
@@ -49,65 +37,57 @@ class CustomInputField extends HookWidget {
       children: [
         Text(
           label,
-          style: AppTypography.inputFont.copyWith(
-            color: AppColors.blackColor,
+          style: TextStyle(
+            color: Colors.black,
             fontSize: 16,
           ),
         ),
         SizedBox(height: 5),
-        Stack(
-          children: [
-            Container(
-              width: 420,
-              decoration: BoxDecoration(
-                color: AppColors.greyColor.withOpacity(0.6),
-                borderRadius: BorderRadius.circular(19),
-              ),
-              child: Row(
-                children: [
-                  if (dropdownItems != null && dropdownItems!.isNotEmpty)
-                    Expanded(
-                      child: Align(
-                        alignment: dropdownAlignment,
-                        child: CustomDropdown(
-                          items: dropdownItems!,
-                          onChanged: handleDropdownChange,
-                          value: selectedDropdownItem.value,
-                        ),
-                      ),
-                    ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 15.0,
-                          right: 8.0), // Add left and right padding here
-                      child: CustomTextFormField(
-                        obscureText: obscureText && !passwordVisible.value,
-                        controller: controller,
-                        validator: validator,
-                        keyboardType: keyboardType,
-                        hint: selectedDropdownItem.value ?? hint,
+        Container(
+          width: 420,
+          decoration: BoxDecoration(
+            color: AppColors.greyColor.withOpacity(0.6),
+            borderRadius: BorderRadius.circular(19),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextFormField(
+                    obscureText: obscureText && !passwordVisible.value,
+                    controller: controller,
+                    validator: validator,
+                    keyboardType: keyboardType,
+                    onChanged: onChanged,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: hint,
+                      isCollapsed: true,
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 19,
+                        horizontal: 8,
                       ),
                     ),
                   ),
-                  if (obscureText)
-                    GestureDetector(
-                      onTap: togglePasswordVisibility,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 15.0),
-                        child: Icon(
-                          passwordVisible.value
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          color: AppColors.greyColor,
-                        ),
-                      ),
-                    ),
-                ],
+                ),
               ),
-            ),
-          ],
+              if (obscureText)
+                GestureDetector(
+                  onTap: togglePasswordVisibility,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 15.0),
+                    child: Icon(
+                      passwordVisible.value
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: AppColors.greyColor,
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ],
     );
