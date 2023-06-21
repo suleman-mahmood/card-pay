ab8ddbdb4dffd9dc5313759de39d6482ec383ee9
Zain Ali Khokhar <zainalikhokhar40@gmail.com>
2023-06-21 18:46:13 +0500

    Updated Marketing Model

--- backend_ddd/marketing/domain/exceptions.py
+++ backend_ddd/marketing/domain/exceptions.py
@@ -1,2 +1,20 @@
 class InvalidReferenceException(Exception):
     """exception raised for invalid reference"""
+
+class InvalidWeightageException(Exception):
+    """exception raised for invalid weightage type"""
+
+class InvalidTrasnsactionTypeException(Exception):
+    """exception raised for invalid transaction type"""
+
+class InvalidCashbackTypeException(Exception):
+    """exception raised for invalid cashback type"""
+
+class NegativeAmountException(Exception):
+    """exception raised for negative amount"""
+
+class InvalidAddingLoyalityPointsException(Exception):
+    """exception raised for invalid adding loyality points"""
+
+class InvalidSlabException(Exception):
+    """exception raised for invalid slab"""
