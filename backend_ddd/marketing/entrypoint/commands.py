feb4cd7bf73c953c71ff5a93e3a29621820ec7a2
Zain Ali Khokhar <zainalikhokhar40@gmail.com>
2023-07-06 15:48:31 +0500

    updating condition in add_loyality_points()

--- backend_ddd/marketing/entrypoint/commands.py
+++ backend_ddd/marketing/entrypoint/commands.py
@@ -4,7 +4,6 @@ from ...entrypoint.uow import AbstractUnitOfWork
 from ...payment.entrypoint import commands as payment_commands
 from ..domain.model import Weightage, CashbackSlab, CashbackType, AllCashbacks
 from ...payment.domain.model import TransactionType
-from ..domain.exceptions import invalid_tx_type_for_loyalty_points_exception
 # Every transaction will check if its a cashback transaction then it'll call the marketing command which will again call the transaction command.
 
 
@@ -64,7 +63,7 @@ def add_loyalty_points(
             transaction_type=transaction_type,
             uow=uow,
         )
-    else:
+    elif transaction_type == TransactionType.P2P_PUSH or transaction_type == TransactionType.P2P_PULL:
         add_loyalty_points_to_user(
             user_id=sender_user_id,
             transaction_amount=transaction_amount,
