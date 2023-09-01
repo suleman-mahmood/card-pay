3c6cfcc404e2bb0f17566247c6718dbc00eb5f6c
Suleman Mahmood <sulemanmahmood99@gmail.com>
2023-09-01 22:27:25 +0500

    wrote script (#234)

---
+++ backend_ddd/scripts/delete_users.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate("credentials-dev.json")
+firebase_admin.initialize_app(cred)
+
+# List all users
+all_users = auth.list_users()
+
+# Delete each user
+for user in all_users.users:
+    try:
+        auth.delete_user(user.uid)
+        print(f"Deleted user: {user.uid}")
+    except auth.AuthError as e:
+        print(f"Error deleting user {user.uid}: {e}")
