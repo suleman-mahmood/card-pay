445e74ad67aad8628b0532444dcf31a5f84394ff
Suleman Mahmood <sulemanmahmood99@gmail.com>
2023-09-07 12:58:08 +0500

    replaced qr package (#270)

--- frontend_futter/lib/src/presentation/views/transaction_service/qr_view.dart
+++ frontend_futter/lib/src/presentation/views/transaction_service/qr_view.dart
@@ -9,78 +9,80 @@ import 'package:cardpay/src/presentation/widgets/navigations/top_navigation.dart
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 @RoutePage()
 class QrView extends HookWidget {
-  const QrView({Key? key}) : super(key: key);
+  QrView({Key? key}) : super(key: key);
+
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
 
   @override
   Widget build(BuildContext context) {
-    final controller = QRCodeDartScanController();
-
-    final qrValue = useState<String>("");
     final cameraAspectRatio = useState<double>(9 / 16);
+
+    void _onQRViewCreated(QRViewController controller) {
+      this.controller = controller;
+      controller.scannedDataStream.listen((scanData) {
+        final qrValue = scanData.code ?? '';
+
+        Map<String, dynamic> jsonMap = {};
+        try {
+          jsonMap = json.decode(qrValue);
+        } on FormatException catch (_) {
+          jsonMap["name"] = "Unknown QR";
+          jsonMap["qr_id"] = '';
+        } on TypeError catch (_) {
+          jsonMap["name"] = "Unknown QR";
+          jsonMap["qr_id"] = '';
+        }
+
+        controller.pauseCamera();
+        context.router
+            .push(SendRoute(
+          uniqueIdentifier: jsonMap["name"],
+          qrId: jsonMap["qr_id"],
+        ))
+            .then(
+          (_) {
+            controller.resumeCamera();
+          },
+        );
+      });
+    }
 
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: [
-            AspectRatio(
-              aspectRatio: cameraAspectRatio.value,
-              child: QRCodeDartScanView(
-                scanInvertedQRCode: true,
-                typeScan: TypeScan.live,
-                controller: controller,
-                onCapture: (Result result) {
-                  qrValue.value = result.text;
-                  Map<String, dynamic> jsonMap = {};
-                  try {
-                    jsonMap = json.decode(result.text);
-                  } on FormatException catch (e) {
-                    jsonMap["name"] = "Unknown QR";
-                    jsonMap["qr_id"] = '';
-                  } on TypeError catch (e) {
-                    jsonMap["name"] = "Unknown QR";
-                    jsonMap["qr_id"] = '';
-                  }
-
-                  if (controller.scanEnabled) {
-                    controller.setScanEnabled(false);
-                    context.router
-                        .push(SendRoute(
-                      uniqueIdentifier: jsonMap["name"],
-                      qrId: jsonMap["qr_id"],
-                    ))
-                        .then(
-                      (_) {
-                        controller.setScanEnabled(true);
-                      },
-                    );
-                  }
-                },
+        body: AspectRatio(
+          aspectRatio: cameraAspectRatio.value,
+          child: Stack(
+            children: [
+              QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
               ),
-            ),
-            PaddingAll(
-              slab: 5,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text('Scan QR', style: AppTypography.mainHeadingWhite),
-                    HeightBox(slab: 1),
-                    Text(
-                      'Place the QR code close to your phone',
-                      style: AppTypography.headingFont,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+              PaddingAll(
+                slab: 5,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Scan QR', style: AppTypography.mainHeadingWhite),
+                      HeightBox(slab: 1),
+                      Text(
+                        'Place the QR code close to your phone',
+                        style: AppTypography.headingFont,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            PaddingHorizontal(slab: 2, child: Header(title: '')),
-          ],
+              PaddingHorizontal(slab: 2, child: Header(title: '')),
+            ],
+          ),
         ),
       ),
     );
