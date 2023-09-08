7e2b00a43f6bdcfaa1b19dda31f90c63505a5695
Shaheer Ahmad <110221120+Shaheer-Ahmd@users.noreply.github.com>
2023-09-08 19:06:21 +0500

    Write Schemas and their tests (#273)
    * added schemas and their tests
    
    * Update backend_ddd/core/api/schemas.py
    
    * incorporated changes
    
    * removed print statement

---
+++ backend_ddd/core/api/schemas.py
@@ -0,0 +1,143 @@
+from core.api import utils
+from typing import List
+from dataclasses import dataclass
+import re
+
+@dataclass()
+class EmailSchema: 
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("Email passed is not a string")
+
+        if not re.match(r"^[a-zA-Z0-9.a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$", self.value):
+            raise utils.CustomException("Invalid Email Passed")
+        return True
+
+@dataclass()
+class PasswordSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value, str):
+            raise utils.CustomException("Password passed is not a string")
+        if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+=-]{8,}$", self.value):
+            raise utils.CustomException("Invalid Password Passed")
+        return True
+
+
+@dataclass()
+class PhoneNumberSchema():
+    value: str
+
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("Phone Number passed is not a string")
+
+        if not re.match(r"^3[0-9]{8}$", self.value):
+            raise utils.CustomException("Invalid Phone Number Passed")
+        return True
+
+
+@dataclass()
+class UserTypeSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("User Type passed is not a string")
+
+        if not re.match(r"^(CUSTOMER|VENDOR|ADMIN|PAYMENT_GATEWAY|CARDPAY)$", self.value):
+            raise utils.CustomException("Invalid User Type Passed")
+        return True
+
+@dataclass()
+class NameSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("Name passed is not a string")
+
+        if not re.match(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$", self.value):
+            raise utils.CustomException("Invalid Name Passed")
+        return True
+
+
+@dataclass()
+class LocationSchema():
+    value: List[float]
+
+    def validate(self):
+        if not isinstance(self.value,list):
+            raise utils.CustomException("Passed Location is not a tuple")
+
+        if len(self.value) != 2:
+            raise utils.CustomException("one or two location coordinates missing")
+
+        if not isinstance(self.value[0],float) or not isinstance(self.value[1],float):
+            raise utils.CustomException("Invalid Location Passed")
+
+        return True
+
+@dataclass()
+class PinSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("Pin passed is not a string")
+
+        if not re.match(r"^[0-9]{4}$", self.value) or self.value == "0000" :
+            raise utils.CustomException("Invalid Pin Passed")
+        return True
+
+@dataclass()
+class OtpSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("OTP passed is not a string")
+
+        if not re.match(r"^[0-9]{4}$", self.value) or self.value == "0000" :
+            raise utils.CustomException("Invalid Otp Passed")
+        return True
+
+@dataclass()
+class UuidSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("UUID passed is not a string")
+
+        if not re.match(r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$", self.value) :
+            raise utils.CustomException("Invalid Uuid Passed")
+        return True
+
+@dataclass()
+class AmountSchema():
+    value: int
+
+    def validate(self):
+        if not isinstance(self.value,int):
+            raise utils.CustomException("Amount passed is not an integer")
+
+        if (self.value<=0 or self.value>=10000):
+            raise utils.CustomException("Invalid Amount Passed")
+        return True
+
+@dataclass()
+class LUMSRollNumberSchema():
+    value: str
+
+    def validate(self):
+        if not isinstance(self.value,str):
+            raise utils.CustomException("Roll number passed is not a string")
+
+        if not (re.match(r"^2[0-9]{7}$",self.value) or re.match(r"^2[0-9]{3}M[0-9]{3}",self.value) or re.match(r"^2[0-9]{3}m[0-9]{3}",self.value)):
+            raise utils.CustomException("Invalid Roll Number Passed")
+        return True
---
+++ backend_ddd/core/api/test_schemas.py
@@ -0,0 +1,59 @@
+from core.api import schemas as sch
+from core.api import utils
+import pytest
+
+def test_validate_payload():
+
+    sample_data = {
+        sch.EmailSchema: {
+            "invalid_inputs": ["","    ", "123123asd", "invalidemail@", "atthedatemissing.com",123123],
+            "valid_inputs": ["shaheer@gmail.com", "another.valid@email.com.pk", "26100279@lums.edu.pk"],
+        },
+        sch.PasswordSchema: {
+            "invalid_inputs": ["","    ", "short", "no_digit",1238719],
+            "valid_inputs": ["StrongP@ssw0rd", "AnotherStr0ng!Password"],
+        },
+        sch.PhoneNumberSchema: {
+            "invalid_inputs": ["","    ", "123456789", "invalid_format",123],
+            "valid_inputs": ["312345678", "333333333"],
+        },
+        sch.UserTypeSchema: {
+            "invalid_inputs": ["","    ", "invalid", "userType", "invalidtype", 2],
+            "valid_inputs": ["CUSTOMER", "VENDOR", "ADMIN"],
+        },
+        sch.NameSchema: {
+            "invalid_inputs": ["","    ", "InvalidName1", "Special%Name",123],
+            "valid_inputs": ["John Doe", "Alice Smith"],
+        },
+        sch.LocationSchema: {
+            "invalid_inputs": ["","    ", [1.2,], [45.678,"Invalid"], "InvalidFormat", 123, [], [1.23,45.678,90.123], {1.23,9.42}],
+            "valid_inputs": [[1.23,45.678], [0.0,0.0]],
+        },
+        sch.PinSchema: {
+            "invalid_inputs": ["","    ", "12345", "Invalid", "0000",1234],
+            "valid_inputs": ["1234", "5678"],
+        },
+        sch.OtpSchema: {
+            "invalid_inputs": ["","    ", "12345", "Invalid", "0000",1234],
+            "valid_inputs": ["1234", "5678"],
+        },
+        sch.UuidSchema: {
+            "invalid_inputs": ["","    ", "invalid-uuid", "12345", "0000",1234],
+            "valid_inputs": ["d13eab16-4b8d-4e44-a9b6-2f47eb059153", "f34c798d-6257-40e5-981c-dde83cb8625f"],
+        },
+        sch.AmountSchema: {
+            "invalid_inputs": ["","    ", 0, 100000000, 5.5, -100],
+            "valid_inputs": [100, 5000, 9999],
+        },
+        sch.LUMSRollNumberSchema: {
+            "invalid_inputs": ["","    ", "1234567", "InvalidFormat", "LUMS12345", "lums12345"],
+            "valid_inputs": ["26100266", "2610M003", "2610m003"],
+        },
+    }
+
+    for schema, data in sample_data.items():
+        for input in data["invalid_inputs"]:
+            with pytest.raises(utils.CustomException):
+                schema(input).validate()
+        for input in data["valid_inputs"]:
+            assert schema(input).validate()
