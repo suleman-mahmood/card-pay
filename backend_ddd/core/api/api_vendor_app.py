d9b04af3038f778cbea9a181eb89f0adca169d56
Malik Muhammad Moaz <40146994+malikmmoaz@users.noreply.github.com>
2023-09-08 22:19:06 +0500

    added vendor api to get vendor balance (#278)
    * added vendor api to get vendor balance
    
    * fixed route and appended prefix
    
    * mrzi hai yaar

--- backend_ddd/core/api/api_vendor_app.py
+++ backend_ddd/core/api/api_vendor_app.py
@@ -5,12 +5,9 @@ from core.api import utils
 from core.entrypoint.uow import UnitOfWork
 from core.payment.entrypoint import queries as payment_qry
 from core.authentication.domain.model import UserType
+from core.authentication.entrypoint import queries as auth_qry
 
-vendor_app = Blueprint("vendor_app", __name__, url_prefix="/api/v1")
-cors = CORS(
-    vendor_app,
-    resources={"/get-vendor-transactions-to-be-reconciled": {"origins": "*"}},
-)
+vendor_app = Blueprint("vendor_app", __name__, url_prefix="/api/v1/vendor-app")
 
 
 @vendor_app.route("/get-vendor-transactions-to-be-reconciled", methods=["GET"])
@@ -30,4 +27,26 @@ def get_vendor_transactions_to_be_reconciled(uid):
         message="All transactions returned successfully",
         status_code=200,
         data=transactions,
+    ).__dict__
+
+
+@vendor_app.route("/get-vendor-balance", methods=["GET"])
+@cross_origin(origin="*", headers=["Authorization"])
+@utils.authenticate_token
+@utils.authenticate_user_type(allowed_user_types=[UserType.VENDOR])
+@utils.user_verified
+def get_vendor_balance(uid):
+    uow = UnitOfWork()
+    balance = auth_qry.get_user_balance(
+        user_id=uid,
+        uow=uow,
+    )
+    uow.close_connection()
+
+    return utils.Response(
+        message="Vendor balance returned successfully",
+        status_code=200,
+        data={
+            "balance": balance,
+        },
     ).__dict__
