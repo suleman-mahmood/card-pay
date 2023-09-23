e832b88f470ffe469b9f8a73a1777300860fd791
Suleman Mahmood <sulemanmahmood99@gmail.com>
2023-09-23 19:22:31 +0500

    new firebase vendor credentaisla and smol query syntax fix

--- backend_ddd/core/payment/entrypoint/queries.py
+++ backend_ddd/core/payment/entrypoint/queries.py
@@ -534,8 +534,8 @@ def vendor_app_get_transactions_to_be_reconciled(
             and sender_wallet_id = %(vendor_id)s;
     """
 
-    uow.dict_cursor.execute(sql, {"vendor_id": vendor_id})
-    row = uow.dict_cursor.fetchone()
+    uow.cursor.execute(sql, {"vendor_id": vendor_id})
+    row = uow.cursor.fetchone()
 
     sql = """
         select 
@@ -555,18 +555,18 @@ def vendor_app_get_transactions_to_be_reconciled(
             inner join users sender on txn.sender_wallet_id = sender.id
             inner join users recipient on txn.recipient_wallet_id = recipient.id
         where
-            txn.recipient_wallet_id = %(vendor_id)s
+            txn.recipient_wallet_id = %s
             and txn.status = 'SUCCESSFUL'::transaction_status_enum
             and txn.transaction_type != 'RECONCILIATION'::transaction_type_enum
     """
 
     if row is not None:
-        last_reconciliation_timestamp = row["max"]
+        last_reconciliation_timestamp = row[0]
         sql += f" and txn.last_updated >= '{str(last_reconciliation_timestamp)}'"
 
     sql += " order by txn.last_updated desc"
 
-    uow.dict_cursor.execute(sql, {"vendor_id": vendor_id})
+    uow.dict_cursor.execute(sql, [vendor_id])
     rows = uow.dict_cursor.fetchall()
 
     transactions = [pmt_vm.TransactionWithIdsDTO.from_db_dict_row(row) for row in rows]
--- vendor-portal/services/initialize-firebase.tsx
+++ vendor-portal/services/initialize-firebase.tsx
@@ -1,33 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
-
-const RUN_DEV = true;
-const RUN_IN_EMULATOR = false;
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCelFqfRRov9iaep88Cjsl_O7pr-Zdx6vU",
-  authDomain: "card-pay-55c1b.firebaseapp.com",
-  projectId: "card-pay-55c1b",
-  storageBucket: "card-pay-55c1b.appspot.com",
-  messagingSenderId: "501478058153",
-  appId: "1:501478058153:web:09130f4d54dc445e8721b8",
-};
-
-const DevfirebaseConfig = {
-  apiKey: "AIzaSyDZMs9HgaJ5UrUXjkgPKQzIeKjHfuoyd9U",
-  authDomain: "card-pay-dev.firebaseapp.com",
-  projectId: "card-pay-dev",
-  storageBucket: "card-pay-dev.appspot.com",
-  messagingSenderId: "451051195028",
-  appId: "1:451051195028:web:e375314f89070d1b600281",
+  apiKey: "AIzaSyAXEhK6txqjYGdCf7i2Mt3OzECAESDupaM",
+  authDomain: "cardpay-1.firebaseapp.com",
+  projectId: "cardpay-1",
+  storageBucket: "cardpay-1.appspot.com",
+  messagingSenderId: "674516928217",
+  appId: "1:674516928217:web:48efa604e11ddeb1d220c8",
+  measurementId: "G-MTM971QTES"
 };
 
 // Initialize Firebase
-const app = initializeApp(RUN_DEV ? DevfirebaseConfig : firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Initialize Auth
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
