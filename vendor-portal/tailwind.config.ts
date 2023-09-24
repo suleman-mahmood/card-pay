6a693b6dc1413342c7babecd1632f4c8acf40bc5
Malik Muhammad Moaz <40146994+malikmmoaz@users.noreply.github.com>
2023-09-24 18:14:57 +0500

    forced light theme

--- vendor-portal/tailwind.config.ts
+++ vendor-portal/tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["light"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "light", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
