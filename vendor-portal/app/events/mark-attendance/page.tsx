65566b5f799a588eb67b2128e8198c1beceac25c
Suleman Mahmood <sulemanmahmood99@gmail.com>
2023-11-02 06:30:41 -0700

    fixed dev to prod url on attendance

--- vendor-portal/app/events/mark-attendance/page.tsx
+++ vendor-portal/app/events/mark-attendance/page.tsx
@@ -14,7 +14,7 @@ import LoadingOverlay from "../spinner";
 const BASE_URL_PROD = 'https://cardpay-1.el.r.appspot.com';
 const BASE_URL_DEV = 'https://dev-dot-cardpay-1.el.r.appspot.com';
 const BASE_URL_LOCAL = 'http://127.0.0.1:5000';
-const BASE_URL = BASE_URL_DEV;
+const BASE_URL = BASE_URL_PROD;
 
 interface MarkAttendanceResponse {
   attendance_data: {
