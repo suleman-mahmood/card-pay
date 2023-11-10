2af82c73723205175fa430fb2dc215e3239dafdd
khuzaima-saeed <khuzaima.saeed@zintechnologies.com>
2023-11-10 23:37:06 +0500

    first two done

--- vendor-portal/app/page.tsx
+++ vendor-portal/app/page.tsx
@@ -2,6 +2,9 @@
 
 import Login from "./components/Login";
 
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" />
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
